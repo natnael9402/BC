146dfca12c1ba756b35335b3a9aa1b482ea05f1d
natnael9402 <natnael9402@gmail.com>
2025-01-27 11:45:59 +0300

    stylesheet api

--- BussinessCard/ProjectCard.jsx
+++ BussinessCard/ProjectCard.jsx
@@ -1,12 +1,27 @@
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, StyleSheet } from "react-native"
 
 const ProjectCard = ({image, text}) => {
     return (
       <View>
-        <Image source={image} style={{ height: 150, aspectRatio: 16 / 9, borderRadius: 10 }} />
-        <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'dimgrey', marginTop: 10 }}>{text}</Text>
+        <Image source={image} style={styles.image} />
+        <Text style={styles.text}>{text}</Text>
       </View>
     )
   }
+
+
+  const styles = StyleSheet.create({
+    image: {
+        height: 150,
+        aspectRatio: 16 / 9,
+        borderRadius: 10
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: 'dimgrey',
+        marginTop: 10
+    }
+  })
 
 export default ProjectCard
